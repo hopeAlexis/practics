7522d2035793cb27d127fb03fbc0fb278c11abc2
hopeAlexis <dododor.mistt@gmail.com>
2023-06-17 07:00:41 +0200

    updated 4 practic

--- 2_sem/practica4/practica4_2.cpp
+++ 2_sem/practica4/practica4_2.cpp
@@ -65,45 +65,46 @@ int main()
 {
 	std::ifstream file; 
 	file.open("appliances.txt");
-	std::vector<Appliance*> appliances;
+	if (file.is_open())
+		std::cout << "The file is open.\n";
 	std::string line;
-	std::getline(file, line);
+		
 
-	if (line == "Electricity monitors") {
-		ElectricityMonitor* electricity = new ElectricityMonitor();
+	std::vector<Appliance*> appliances;
+
+	while(!file.eof())
+	{
 		std::getline(file, line);
-		electricity->setName(line);
-		appliances.push_back(electricity);
-	}
-	else if (line == "Discrete signal input blocks") {
-		DiscreteSignalInputBlock* signal = new DiscreteSignalInputBlock();
-		std::getline(file, line);
-		signal->setName(line);
-		appliances.push_back(signal);
-	}
-	else if (line == "Heating control blocks") {
-		HeatingControlBlock* heating = new HeatingControlBlock();
-		std::getline(file, line);
-		heating->setName(line);
-		appliances.push_back(heating);
-	}
-	else {
-		std::cout << "No corresponding appliances." << line << std::endl;
+		if (line == "Electricity monitors") {
+			ElectricityMonitor* electricity = new ElectricityMonitor();
+			std::getline(file, line);
+			electricity->setName(line);
+			appliances.push_back(electricity);
+		}
+		else if (line == "Discrete signal input blocks") {
+			DiscreteSignalInputBlock* signal = new DiscreteSignalInputBlock();
+			std::getline(file, line);
+			signal->setName(line);
+			appliances.push_back(signal);
+		}
+		else if (line == "Heating control blocks") {
+			HeatingControlBlock* heating = new HeatingControlBlock();
+			std::getline(file, line);
+			heating->setName(line);
+			appliances.push_back(heating);
+		}
+		else {
+			std::cout << "No corresponding appliances for " << line << " line.\n";
+		}
 	}
 
 	file.close();
 
 	for (Appliance* appliance : appliances)		//for each item in the vector
 	{
-		std::cout << "1\n";
+		std::cout << "\n";
 		appliance->poll();
 	}
-
-	//for (int i = 0; i < appliances.size(); i++)
-	//{
-	//	std::cout << "1\n";
-	//	appliances[i]->poll();
-	//}
 
 	for (Appliance* device : appliances) 
 	{
